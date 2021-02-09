3943eb40f05f4890709ca83a0594348a11b94955
jordan62300 <jordanfievetpro@outlook.fr>
2021-02-09 20:46:35 +0100

    ajout langue francaise

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@ export default {
   },
   methods: {
     fetchWeather() {
-      fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+      fetch(`${this.url_base}weather?q=${this.query}&units=metric&lang=fr&APPID=${this.api_key}`)
           .then(res => {
             return res.json();
           }).then(this.setResults);
